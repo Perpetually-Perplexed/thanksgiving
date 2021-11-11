b9131e917b440a14ed6da9f3dcdd4026103b2a16
BuildTools <unconfigured@null.spigotmc.org>
2021-11-11 11:14:38 +0530

    Add Missions Enum

---
+++ src/main/java/me/perplexed/delivery/mission/Missions.java
@@ -0,0 +1,24 @@
+package me.perplexed.delivery.mission;
+
+import me.perplexed.delivery.Main;
+import me.perplexed.delivery.items.Items;
+import org.bukkit.Material;
+import org.bukkit.StructureType;
+import org.bukkit.inventory.ItemStack;
+
+public enum Missions {
+    VILLAGE(5,600,StructureType.VILLAGE,new ItemStack(Material.SWEET_BERRIES,30)
+            ,new ItemStack(Material.COOKED_CHICKEN,16), Items.getInstance().DISCOUNT_TURKEY.getCustomItem());
+
+    private final int difficulty; // 1 is the lowest 5 is the highest
+    private final long time; // time is in ticks
+    private final ItemStack[] resourcesRequired;
+    private final StructureType type;
+
+    Missions(int difficulty,long time, StructureType type, ItemStack... itemsRequired) {
+        this.time = time;
+        this.type = type;
+        this.difficulty = difficulty;
+        this.resourcesRequired = itemsRequired.length == 0 ? new ItemStack[]{new ItemStack(Material.COOKED_CHICKEN)} : itemsRequired;
+    }
+}
