ab8ad33cd03fb4bceb0c53675e1852cc2ae44805
BuildTools <unconfigured@null.spigotmc.org>
2021-11-11 09:59:45 +0530

    Add Instance Singleton

--- src/main/java/me/perplexed/delivery/Main.java
+++ src/main/java/me/perplexed/delivery/Main.java
@@ -4,14 +4,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    private static Main inst;
+    
     @Override
     public void onEnable() {
+        inst = this;
         // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Main getInstance() {
+        return inst;
     }
 }
