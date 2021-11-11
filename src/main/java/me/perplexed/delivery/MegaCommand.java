fed6a798905a2dd5aa093b7a1cb54bf1aa780d62
BuildTools <unconfigured@null.spigotmc.org>
2021-11-11 11:14:18 +0530

    Finish tab complete, Add permission checks, Argument checks

--- src/main/java/me/perplexed/delivery/MegaCommand.java
+++ src/main/java/me/perplexed/delivery/MegaCommand.java
@@ -1,9 +1,11 @@
 package me.perplexed.delivery;
 
 import me.perplexed.delivery.utils.TxtUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.craftbukkit.v1_17_R1.CraftServer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -21,9 +23,54 @@ public class MegaCommand implements TabExecutor {
             return true;
         }
 
+        if (!pl.hasPermission("delivery.start")) {
+            pl.sendMessage("Sorry, you don't have permission to run this command!");
+            return true;
+        }
+
         if (args.length == 0) {
             pl.sendMessage(TxtUtils.red("Usage: /deliveryman <solo/team/versus>"));
             return true;
+        }
+
+        //todo add mission support
+        switch (args[0].toLowerCase()) {
+            case "solo" -> {
+
+            }
+
+            case "team" -> {
+                if (args.length > 2) {
+                    pl.sendMessage(TxtUtils.red("Who are you teaming with?"));
+                    return false;
+                }
+
+                for (int i = 1;i < args.length; ++i) {
+                    Player team = Bukkit.getPlayer(args[1]);
+                    if (team == null) {
+                        pl.sendMessage("\"" +args[1] + "\" is not a real player or is offline!");
+                        return true;
+                    }
+                }
+            }
+
+            case "versus","vs" -> {
+                if (args.length > 2) {
+                    pl.sendMessage(TxtUtils.red("Who are you battling with?"));
+                    return false;
+                }
+
+                for (int i = 1;i < args.length; ++i) {
+                    Player team = Bukkit.getPlayer(args[1]);
+                    if (team == null) {
+                        pl.sendMessage("\"" +args[1] + "\" is not a real player or is offline!");
+                        return true;
+                    }
+                }
+
+
+            }
+
         }
 
         return false;
@@ -32,8 +79,24 @@ public class MegaCommand implements TabExecutor {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
         List<String> tabs = new ArrayList<>();
-        tabs.add("solo"); tabs.add("team"); tabs.add("versus");
+        if (!sender.hasPermission("delivery.start")) {
+            return tabs;
+        }
 
+        tabs.add("solo"); tabs.add("team"); tabs.add("versus"); tabs.add("vs");
+        if (args.length == 1) {
+            List<String> otherReturn = new ArrayList<>();
+            for (String s : tabs) {
+                if (s.toLowerCase().startsWith(args[0].toLowerCase())) {
+                    otherReturn.add(s);
+                }
+            }
+            return otherReturn;
+        }
+
+        if (args.length == 2) {
+            return null;
+        }
 
         return tabs;
     }
