17f61a2e7260ef3ae9162dd127af048b4bf28f26
BuildTools <unconfigured@null.spigotmc.org>
2021-11-14 19:41:06 +0530

    Register commands

--- src/main/java/me/perplexed/delivery/Main.java
+++ src/main/java/me/perplexed/delivery/Main.java
@@ -1,14 +1,30 @@
 package me.perplexed.delivery;
 
+import me.perplexed.delivery.utils.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     private static Main inst;
+    // temp
+    private int taskId;
     
     @Override
     public void onEnable() {
         inst = this;
+        getCommand("delivery").setExecutor(new MegaCommand());
+        getCommand("delivery").setTabCompleter(new MegaCommand());
+        getCommand("testparticel").setExecutor((sender, command, label, args) -> {
+            if (!(sender instanceof Player pl)) return true;
+            try {
+                taskId = Utils.spawnBeam(pl.getLocation(),Long.parseLong(args[0]),pl.getUniqueId());
+            } catch (NumberFormatException e) {
+                Bukkit.getScheduler().cancelTask(taskId);
+            }
+            return true;
+        });
     }
 
 
