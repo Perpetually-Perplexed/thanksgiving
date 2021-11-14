00fb1d6c4cf4e9da273efe71940b5fc96c4006fc
BuildTools <unconfigured@null.spigotmc.org>
2021-11-14 19:37:20 +0530

    Made Turkey easier to get

--- src/main/java/me/perplexed/delivery/items/DiscountTurkey.java
+++ src/main/java/me/perplexed/delivery/items/DiscountTurkey.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.Beacon;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -20,9 +21,11 @@ public class DiscountTurkey extends Item {
         setCustomItem(discount);
 
         ShapedRecipe recipe = new ShapedRecipe(key,discount);
-        recipe.shape("HCH","CCC","HCH");
+        recipe.shape("PHP","CCC","FCF");
         recipe.setIngredient('H',Material.HONEY_BOTTLE);
         recipe.setIngredient('C',new ItemStack(Material.COOKED_CHICKEN,2));
+        recipe.setIngredient('F',Material.FLINT_AND_STEEL);
+        recipe.setIngredient('P',Material.POTATO);
 
         Bukkit.getServer().addRecipe(recipe);
     }
