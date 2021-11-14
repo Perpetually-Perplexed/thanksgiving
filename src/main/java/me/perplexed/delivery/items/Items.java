8cb096eceae3912ec39bf39199120866e5718bdd
BuildTools <unconfigured@null.spigotmc.org>
2021-11-14 19:39:41 +0530

    Add custom items

--- src/main/java/me/perplexed/delivery/items/Items.java
+++ src/main/java/me/perplexed/delivery/items/Items.java
@@ -1,5 +1,7 @@
 package me.perplexed.delivery.items;
 
+import org.checkerframework.checker.units.qual.C;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,8 +11,10 @@ public class Items {
     private final static Items inst = new Items();
 
     public final Item DISCOUNT_TURKEY = new DiscountTurkey();
+    public final Item CRANBERRY = new Cranberry();
+    public final Item CRANBERRY_JUICE = new CranberryJuice();
 
-    private final List<Item> items = Arrays.asList(DISCOUNT_TURKEY);
+    private final List<Item> items = Arrays.asList(DISCOUNT_TURKEY,CRANBERRY,CRANBERRY_JUICE);
 
     public List<Item> getItems() {
         return items;
